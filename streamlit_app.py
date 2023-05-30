bad21cadebb9cbb05d864a25c3bad4bc15ea33ee
ShriyaRandhar <135001969+ShriyaRandhar@users.noreply.github.com>
2023-05-30 16:03:55 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled free Range Eggs')
