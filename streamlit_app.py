332ee2c96f19aa266d70c458a6b0159a77c01562
ShriyaRandhar <135001969+ShriyaRandhar@users.noreply.github.com>
2023-06-01 20:55:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,8 @@ if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
-    
+  
+streamlit.stop()
 
 #my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 #streamlit.dataframe(my_data_rows)
